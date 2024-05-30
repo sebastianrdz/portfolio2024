d4c2466fe123d7c37b30877e99c36db3013ed804
Sebastian Rodriguez <sebasstian.rdz@gmail.com>
2024-05-29 18:10:04 -0600

    feat: add more about me section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 export default function AboutPage() {
-  const { welcome, education } = aboutData;
+  const { welcome, education, more } = aboutData;
   const textBoxClassName =
     "grid bg-primary-foreground w-fill min-h-[500px] content-center justify-center p-8 xl:p-32";
 
@@ -14,6 +14,7 @@ export default function AboutPage() {
   return (
     <section id="about" className="container mx-auto mt-32 my-8">
       <div className="grid grid-cols-1 md:grid-cols-2">
+        {/* welcome section */}
         <div className={cn(imageBoxClassName, "bg-primary")}>
           <Icons.logo className="aspect-square w-48 h-48 p-4 text-secondary" />
         </div>
@@ -21,11 +22,12 @@ export default function AboutPage() {
           <h1 className="text-2xl tracking-widest mb-4">{welcome.title}</h1>
           <p>{welcome.description}</p>
         </div>
+
+        {/* education section */}
         <div className={textBoxClassName}>
           <h1 className="text-2xl tracking-widest mb-4">{education.title}</h1>
           <p>{education.description}</p>
         </div>
-
         <div className={cn(imageBoxClassName, "hidden md:flex")}>
           <Image
             src="/about-1.jpg"
@@ -34,8 +36,15 @@ export default function AboutPage() {
             fill
           />
         </div>
+
+        {/* more about me section */}
         <div className={cn(textBoxClassName, "md:col-span-2")}>
-          <h1 className="text-2xl tracking-widest mb-4"></h1>
+          <h1 className="text-2xl tracking-widest mb-4">{more.title}</h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+            <p className="md:col-span-2">{more.text[0]}</p>
+            <p>{more.text[1]}</p>
+            <p>{more.text[2]}</p>
+          </div>
         </div>
       </div>
     </section>
--- lib/data.tsx
+++ lib/data.tsx
@@ -1,24 +1,5 @@
 import { Icons } from "@/components/assets/icons";
-
-const data = {
-  about:
-    "This is a simple API for testing purposes. It returns a random number between 1 and 100.",
-  contact: "",
-  portfolio: {
-    name: "Portfolio",
-    url: "",
-    projects: [
-      {
-        name: "Project 1",
-        url: "",
-      },
-      {
-        name: "Project 2",
-        url: "",
-      },
-    ],
-  },
-};
+import { title } from "process";
 
 export const aboutData = {
   welcome: {
@@ -26,12 +7,22 @@ export const aboutData = {
       "Hey, I'm Sebastian, a Full-Stack Software Developer from Monterrey, Mexico.",
     description:
       "I've been designing and developing software for over 2 years, working alongside visionaries ranging from startup founders to experienced engineers. I specialize in front-end development and overall user experience, bringing visions to life from initial concepts to fully functional applications.",
-    imgSrc: <Icons.logo />,
+    icon: <Icons.logo />,
+    imgSrc: "",
   },
   education: {
     title: "Education",
     description:
       "I hold a Bachelor's degree in Computer Science and Technology from Tecnologico de Monterrey. My education provided a solid foundation in programming languages and software development methodologies. During my studies, I gained hands-on experience through projects and internships, applying theoretical knowledge to real-world scenarios. This academic background has been essential in shaping my skills and passion for software development.",
+    icon: null,
     imgSrc: "/about-1.jpg",
+  },
+  more: {
+    title: "More About Me",
+    text: [
+      "My journey into software development began in high school with a coding class that introduced me to programming through block-based coding and game creation. Captivated by math, problem-solving, and the potential of technology, I pursued a career as a Full-Stack Software Developer. I thoroughly enjoy building fully functional applications from scratch, with a particular passion for front-end development and bringing projects to life visually and interactively.",
+      "Outside of work, I enjoy going to the gym, meeting new people, painting, music, and reading. These activities keep me balanced, continually learning, and creatively inspired, enhancing my professional work.",
+      "I aspire to become an entrepreneur and establish my own startups and businesses. My ultimate goal is to achieve a level of freedom that allows me to enjoy the little things in life, give back to my family and friends, and live a fulfilling, care-free life.",
+    ],
   },
 };
